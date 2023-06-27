c331f7f38650ac6833517280ba47396b64544555
jatin sidana <jatinsadana1998@gmail.com>
2023-06-27 15:25:34 +0530

    more git info

--- iletsproject/src/pages/About.js
+++ iletsproject/src/pages/About.js
@@ -1,6 +1,17 @@
 export const About=()=>{
     return(
         <>HelloWorld!
+        <h1>
+            how are uh aman 
+        </h1>
+        <p>
+            have u understand github.
+            git add .
+            git commit -m ""
+            git push
+             
+             git pull when u start the project
+        </p>
         </>
     )
 }
