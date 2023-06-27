8eeba129a5a34d2a688b1c238a2ea34566ea85b7
amandhillon22 <a.dhillon9779@gmail.com>
2023-06-27 15:15:43 +0530

    about component create

--- iletsproject/src/App.js
+++ iletsproject/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Home} from './pages/Home';
+import { About } from './pages/About';
 
 
 
@@ -9,7 +10,7 @@ function App() {
             <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path='/' element={<Home/>}/>
-            
+                    <Route path='/about' element={<About/>}/>            
                 </Routes>
             </BrowserRouter>
         </>
