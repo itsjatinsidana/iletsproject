6a57f3d03927458337cbec39bacabe595aa9356f
amandhillon22 <a.dhillon9779@gmail.com>
2023-07-08 15:47:11 +0530

    spoken page made

--- iletsproject/src/pages/Spoken.js
+++ iletsproject/src/pages/Spoken.js
@@ -1,197 +1,263 @@
 export const Spoken = () => {
-    return (
-      <>
-        {/* ***** Header Area Start ***** */}
-        <header className="header-area header-sticky">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <nav className="main-nav">
-                  {/* ***** Logo Start ***** */}
-                  <a href="\" className="logo">
-                    <img src="..\assets\images\BhashaLogo.png" alt="Bhasha"></img>
-                  </a>
-                  {/* ***** Logo End ***** */}
-                  {/* ***** Menu Start ***** */}
-                  <ul className="nav">
-                    <li>
-                      <a href="\">Home</a>
-                    </li>
-                    <li>
-                      <a href="\courses" className="active">
-                        Courses
+  return (
+    <>
+      {/* ***** Header Area Start ***** */}
+      <header className="header-area header-sticky">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <nav className="main-nav">
+                {/* ***** Logo Start ***** */}
+                <a href="\" className="logo">
+                  <img src="..\assets\images\BhashaLogo.png" alt="Bhasha"></img>
+                </a>
+                {/* ***** Logo End ***** */}
+                {/* ***** Menu Start ***** */}
+                <ul className="nav">
+                  <li>
+                    <a href="\">Home</a>
+                  </li>
+                  <li>
+                    <a href="\courses" className="active">
+                      Courses
+                    </a>
+                  </li>
+                  <li>
+                    <a href="\coursedetail">Course Details</a>
+                  </li>
+                  <li className="has-sub">
+                    <a href="javascript:void(0)">Pages</a>
+                    <ul className="sub-menu">
+                      <li>
+                        <a href="meetings.html">Upcoming Meetings</a>
+                      </li>
+                      <li>
+                        <a href="meeting-details.html">Meeting Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="index.html">Courses</a>
+                  </li>
+                  <li>
+                    <a href="index.html">Contact Us</a>
+                  </li>
+                </ul>
+                <a className="menu-trigger">
+                  <span>Menu</span>
+                </a>
+                {/* ***** Menu End ***** */}
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* ***** Header Area End ***** */}
+      <section className="heading-page header-text" id="top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h6> </h6>
+              <h2> Courses </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="meetings-page" id="meetings">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="meeting-single-item">
+                    <div className="thumb">
+                      <a href="meeting-details.html">
+                        <img src="assets/images/spokencd.svg" alt="" />
                       </a>
-                    </li>
-                    <li>
-                      <a href="\coursedetail">Course Details</a>
-                    </li>
-                    <li className="has-sub">
-                      <a href="javascript:void(0)">Pages</a>
-                      <ul className="sub-menu">
-                        <li>
-                          <a href="meetings.html">Upcoming Meetings</a>
-                        </li>
-                        <li>
-                          <a href="meeting-details.html">Meeting Details</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href="index.html">Courses</a>
-                    </li>
-                    <li>
-                      <a href="index.html">Contact Us</a>
-                    </li>
-                  </ul>
-                  <a className="menu-trigger">
-                    <span>Menu</span>
-                  </a>
-                  {/* ***** Menu End ***** */}
-                </nav>
-              </div>
-            </div>
-          </div>
-        </header>
-        {/* ***** Header Area End ***** */}
-        <section className="heading-page header-text" id="top">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h6>  </h6>
-                <h2> Courses </h2>
-              </div>
-            </div>
-          </div>
-        </section>
-  
-        <section className="meetings-page" id="meetings">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="meeting-single-item">
-                      <div className="thumb">
-                        <a href="meeting-details.html">
-                          <img src="assets/images/spokencd.svg" alt="" />
-                        </a>
+                    </div>
+                    <div className="down-content">
+                      <h2>SPOKEN ENGLISH (SE)</h2>
+                      <p>
+                        <b>
+                          To convey message from one person to another, we use
+                          Spoken English. It is a way of communication. The
+                          dialog or conversation between two person or among
+                          group, is Spoken English. Now a days, english
+                          communication or Spoken English has become very
+                          important in business or client dealing. To speak
+                          English fluently, you need to read, listen and speak.
+                          Self talk is very important if you want to speak
+                          English fluently.
+                        </b>{" "}
+                        <br />
+                      </p>
+
+                      <div className="b-example-divider" />
+                      <div className="container px-4 py-5" id="icon-grid">
+                        <h2 className="pb-2 border-bottom">
+                          Steps to speak English fluently:
+                        </h2>
+                        <ul className="list-group">
+                          <li className="list-group-item">
+                            Speak a little English everyday
+                          </li>
+                          <li className="list-group-item">
+                            Train yourself to think in English
+                          </li>
+                          <li className="list-group-item">
+                            Don't be afraid to make mistakes
+                          </li>
+                          <li className="list-group-item">
+                            Read English newspaper, watch English movies with
+                            subtitles, listen English songs
+                          </li>
+                          <li className="list-group-item">
+                            Listen English sentences and repeat it
+                          </li>
+                          <li className="list-group-item">Be motivated</li>
+                        </ul>
                       </div>
-                      <div className="down-content">
-                        <h2>PEARSON TEST OF ENGLISH (PTE)</h2>
-                        <p>
-                          <b>
-                            PTE is a world-leading provider of secure English
-                            language tests
-                          </b>{" "}
-                          <br /> This test provides secure English language
-                          testing for study applications worldwide, and for visa
-                          applications for work and migration in Australia, the
-                          UK, New Zealand, and Canada. Every year, tens of
-                          thousands of people trust PTE to help them rapidly prove
-                          their English proficiency – and to open doors to their
-                          future lives and careers.
-                        </p>
-                        <figure className="text-center">
-                          <blockquote className="blockquote">
-                            <br />
-                            <br />
-                            <h2>Three parts of the test</h2>
-                          </blockquote>
-  
-                          <figure className="text-center">
-                            <blockquote className="blockquote">
-                              <p>
-                                <h6>
-                                  There are 20 different question types in the
-                                  test, ranging from multiple choice through to
-                                  essay writing. Make sure you review the
-                                  different question formats ahead of your test,
-                                  as each part has different types of questions.
-                                </h6>
-                              </p>
-                            </blockquote>
-                          </figure>
-                        </figure>
-                        <div className="why_ielts_tp">
-                          <div className="row">
-                            <div className="col-sm-4">
-                              <div className="why_ielts_bx">
-                                <img
-                                  src="../assets/images/part3icons.svg"
-                                  alt=""
-                                />
-                                <h4>Part 1: Speaking & Writing </h4>
-                                <p>
-                                  This part of the test contains seven different
-                                  question types. You will be tested on both your
-                                  speaking and writing skills, and will use
-                                  English you might hear in an academic
-                                  environment.
-                                </p>
-                              </div>
-                            </div>
-                            <div className="col-sm-4">
-                              <div className="why_ielts_bx">
-                                <img
-                                  src="../assets/images/part2icons.svg"
-                                  alt=""
-                                />
-                                <h4>Reading</h4>
-                                <p>
-                                  This part of the test contains five different
-                                  question types, and tests your ability to read
-                                  academic English. One item type assesses both
-                                  writing and reading skills.
-                                </p>
-                              </div>
-                            </div>
-                            <div className="col-sm-4">
-                              <div className="why_ielts_bx">
-                                <img
-                                  src="../assets/images/3particon.svg"
-                                  alt=""
-                                />
-                                <h4>Listening</h4>
-                                <p>
-                                  This part of the test contains eight question
-                                  types and tests your listening skills, through a
-                                  series of audio and video clips.
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        
-                        </div>
+                      <br />
+                      <br />
+
+                      <h3>
+                        <u>SPOKEN ENGLISH SERVED BY BHASHA:</u>
+                      </h3>
+                      <br />
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <td>Sr. No.</td>
+                            <td>Our Salient Courses</td>
+                            <td>Description</td>
+                            <td>Duration</td>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr className="table-active">
+                            <td>1.</td>
+                            <td>Basic & Spoken English</td>
+                            <td>
+                              <dl>
+                                <dt>We Offer:</dt>
+                                <dd>
+                                  <ul>
+                                    <li>
+                                      Sound and pronunciation of alphabets
+                                    </li>
+                                    <li>
+                                      Listening and Reading skills practice
+                                    </li>
+                                    <li>Vocabulary of daily use</li>
+                                    <li>Introduction to Parts of Speech</li>
+                                    <li>Verbs forms and meaning</li>
+                                    <li>Basic questions and answers</li>
+                                    <li>
+                                      Command, Direction, Food order, etc.
+                                    </li>
+                                    <li>Subject and Verb: word order</li>
+                                    <li>Sentences formation</li>
+                                    <li>Basic dialogue writing</li>
+                                    <li>Introduction and Role play practice</li>
+                                    <li>Basic conversation skills</li>
+                                  </ul>
+                                </dd>
+                              </dl>
+                            </td>
+                            <td>
+1-6 MONTHS
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td>2.</td>
+                            <td>Advanced English</td>
+                            <td>
+                              <dl>
+                                <dt>We Offer:</dt>
+                                <dd>
+                                  <ul>
+                                    <li>Introduction to English Grammar</li>
+                                    <li>Parts of Speech</li>
+                                    <li>Time and Tenses</li>
+                                    <li>Modal Verbs</li>
+                                    <li>Active and Passive Voice</li>
+                                    <li>Conditional Sentences</li>
+                                  </ul>
+                                </dd>
+                              </dl>
+
+                              <dl>
+                                <dt>Activties Included:</dt>
+                                <dd>
+                                  <ul>
+                                    <li>Role play / Drama / Games</li>
+                                    <li>Tongue twister practice</li>
+                                    <li>Paraphrasing of articles or stories</li>
+                                    <li>Story telling / Picture describing</li>
+                                    <li>Dialog drill / Conversation</li>
+                                    <li>Debate / Group discussion</li>
+                                    <li>
+                                      Use of Gap fillers / contracted forms
+                                    </li>
+                                  </ul>
+                                </dd>
+                              </dl>
+                            </td>
+                            <td>2 MONTHS</td>
+                          </tr>
+
+                          <tr className="table-active">
+                            <td>3.</td>
+                            <td>Business English</td>
+                            <td>
+                              <dl>
+                                <dt>1.Grammar:</dt>
+                                <dd>
+                                  e.g. tenses, word order, verb patterns,<br/> Modal
+                                  Auxiliaries, Passive voice,<br/> Conditional
+                                  Sentences, relative clauses
+                                </dd>
+                                <dt>2. Lexis:</dt>
+                                <dd>
+                                  e.g. Business world vocabulary, idioms,<br/>
+                                  expressions, collocation
+                                </dd>
+                              </dl>
+                            </td>
+                            <td>2 MONTHS</td>
+                          </tr>
+                        </tbody>
+                      </table>
                       
-                        
-                      </div>
                     </div>
                   </div>
-                  <div className="col-lg-12">
-                    <div className="main-button-red">
-                      <a href="/courses">Back To Meetings List</a>
-                    </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="main-button-red">
+                    <a href="/courses">Back To Meetings List</a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="footer">
-            <p>
-              Copyright © 2022 Edu Meeting Co., Ltd. All Rights Reserved.
-              <br />
-              Design:{" "}
-              <a
-                href="https://templatemo.com"
-                target="_parent"
-                title="free css templates"
-              >
-                TemplateMo
-              </a>
-            </p>
-          </div>
-        </section>
-      </>
-    );
-  };
-  
+        </div>
+        <div className="footer">
+          <p>
+            Copyright © 2022 Edu Meeting Co., Ltd. All Rights Reserved.
+            <br />
+            Design:{" "}
+            <a
+              href="https://templatemo.com"
+              target="_parent"
+              title="free css templates"
+            >
+              TemplateMo
+            </a>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
