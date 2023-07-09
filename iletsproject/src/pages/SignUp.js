7ef3f5ba22f7bd6f9ac21b22cc47a2f70e1060af
amandhillon22 <a.dhillon9779@gmail.com>
2023-07-10 01:33:11 +0530

    sign in page made

--- iletsproject/src/pages/SignUp.js
+++ iletsproject/src/pages/SignUp.js
@@ -1,46 +1,120 @@
 export const SignUp = () => {
-    return(
-        <>
-        <main className="form-signin">
-  <form>
-    <img
-      className="mb-4"
-      src="/docs/5.0/assets/brand/bootstrap-logo.svg"
-      alt=""
-      width={72}
-      height={57}
-    />
-    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-    <div className="form-floating">
-      <input
-        type="email"
-        className="form-control"
-        id="floatingInput"
-        placeholder="name@example.com"
-      />
-      <label htmlFor="floatingInput">Email address</label>
-    </div>
-    <div className="form-floating">
-      <input
-        type="password"
-        className="form-control"
-        id="floatingPassword"
-        placeholder="Password"
-      />
-      <label htmlFor="floatingPassword">Password</label>
-    </div>
-    <div className="checkbox mb-3">
-      <label>
-        <input type="checkbox" defaultValue="remember-me" /> Remember me
-      </label>
-    </div>
-    <button className="w-100 btn btn-lg btn-primary" type="submit">
-      Sign in
-    </button>
-    <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
-  </form>
-</main>
+  return (
+    <>
 
-        </>
-    )
-}
+          {/* ***** Header Area Start ***** */}
+          <header className="header-area header-sticky">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <nav className="main-nav">
+                {/* ***** Logo Start ***** */}
+                <a href="\" className="logo">
+                  <img src="..\assets\images\BhashaLogo.png" alt="Bhasha"></img>
+                </a>
+                {/* ***** Logo End ***** */}
+                {/* ***** Menu Start ***** */}
+                <ul className="nav">
+                  <li>
+                    <a href="\">Home</a>
+                  </li>
+                  <li>
+                    <a href="\courses" className="active">
+                      Courses
+                    </a>
+                  </li>
+                  <li>
+                    <a href="\coursedetail">Course Details</a>
+                  </li>
+                  <li className="has-sub">
+                    <a href="javascript:void(0)">Pages</a>
+                    <ul className="sub-menu">
+                      <li>
+                        <a href="meetings.html">Upcoming Meetings</a>
+                      </li>
+                      <li>
+                        <a href="meeting-details.html">Meeting Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="index.html">Courses</a>
+                  </li>
+                  <li>
+                    <a href="index.html">Contact Us</a>
+                  </li>
+                </ul>
+                <a className="menu-trigger">
+                  <span>Menu</span>
+                </a>
+                {/* ***** Menu End ***** */}
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* ***** Header Area End ***** */}
+      <section className="heading-page header-text" id="top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h6>  </h6>
+              <h2> Courses </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <br/>
+      <br/>
+      <br/>
+      <div className="container">
+        <div className="row">
+            <div className="col-12">
+      <form className="formSI cardSI">
+        <div className="card_headerSI">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={24}
+            height={24}
+          >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path
+              fill="currentColor"
+              d="M4 15h2v5h12V4H6v5H4V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6zm6-4V8l5 4-5 4v-3H2v-2h8z"
+            />
+          </svg>
+          <h1 className="form_headingSI">Sign in</h1>
+        </div>
+        <div className="fieldSI">
+          <label htmlFor="username">Username</label>
+          <input
+            className="inputSI"
+            name="username"
+            type="text"
+            placeholder="Username"
+            id="username"
+          />
+        </div>
+        <div className="fieldSI">
+          <label htmlFor="password">Password</label>
+          <input
+            className="inputSI"
+            name="user_password"
+            type="password"
+            placeholder="Password"
+            id="password"
+          />
+        </div>
+        <div className="fieldSI">
+          <button className="button">Login</button>
+        </div>
+        
+      </form>
+      </div>
+      </div>
+      </div>
+    </>
+  );
+};
